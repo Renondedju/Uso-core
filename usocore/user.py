7512416f71eb8ecf70739d769f413d58d4b36787
Renondedju <lefantomedelaredstone@gmail.com>
2018-07-17 14:31:16 +0200

    Added every field requiered into user and beatmap

--- usocore/user.py
+++ usocore/user.py
@@ -21,10 +21,37 @@
 # SOFTWARE.
 
 import pyosu
+from datetime import datetime
 
 class User():
     
-    def __init__(self, apiuser = pyosu.User):
+    def __init__(self, apiuser = pyosu.models.User):
         
-        pass
+        self.apisuer = apiuser
+
+        #User performances
+        self.accuracy_average   = 0
+        self.pp_average         = 0
+        self.bpm_low            = 0
+        self.bpm_average        = 0
+        self.bpm_high           = 0
+        self.od_average         = 0
+        self.ar_average         = 0
+        self.cs_average         = 0
+        self.len_average        = 0   # Drain
+        self.playstyle          = 0.5 # Jumps 0 -----|----- 1 Stream
+        self.last_update        = datetime(1970, 1, 1)
+
+        # This is the last core patch used to display updates if needed
+        self.last_patch_used    = "0.0.0"
+
+        # Every value in the dict is an int value
+        # {HD : 20, HDDT : 60, ...}
+        self.playrate    = {}
+
+        # Every value in the dict is an int value
+        # mods :  beatmaps id recommended in this mods
+        #  {HD : [123456, 456789, 132456, ...], HDDT : [...], ...}
+        self.recommended = {}
+
     
