3adbdf89979dd638571daebffe5330c7ee0f584e
Renondedju <lefantomedelaredstone@gmail.com>
2018-07-18 15:07:01 +0200

    Done user and beatmap models

--- usocore/models/user.py
+++ usocore/models/user.py
@@ -24,51 +24,41 @@ import pyosu
 
 from datetime import datetime
 
-from osucore.db import db
+from usocore.db import db
 
 class User(db.Model):
     __tablename__ = 'users'
 
     user_id            = db.Column(db.Integer())
-    username           = db.Column(db.String())
     count300           = db.Column(db.Integer())
     count100           = db.Column(db.Integer())
     count50            = db.Column(db.Integer())
     playcount          = db.Column(db.Integer())
-    ranked_score       = db.Column(db.Float())
-    total_score        = db.Column(db.Float())
     pp_rank            = db.Column(db.Integer())
-    level              = db.Column(db.Float())
-    pp_raw             = db.Column(db.Float())
-    accuracy           = db.Column(db.Float())
     count_rank_ss      = db.Column(db.Integer())
     count_rank_ssh     = db.Column(db.Integer())
+    count_rank_a       = db.Column(db.Integer())
     count_rank_s       = db.Column(db.Integer())
     count_rank_sh      = db.Column(db.Integer())
-    count_rank_a       = db.Column(db.Integer())
-    country            = db.Column(db.String())
     pp_country_rank    = db.Column(db.Integer())
-    #events            = db.Column(db.) No idea of how to do this since events are a list of the user_event model
     accuracy_average   = db.Column(db.Integer())
-    pp_average         = db.Column(db.Float())
-    bpm_low            = db.Column(db.Float())
-    bpm_average        = db.Column(db.Float())
-    bpm_high           = db.Column(db.Float())
-    od_average         = db.Column(db.Float())
-    ar_average         = db.Column(db.Float())
-    cs_average         = db.Column(db.Float())
-    len_average        = db.Column(db.Float())
-    playstyle          = db.Column(db.Float())
-    last_update        = db.Column(db.Date())
-    last_patch_used    = db.Column(db.String())
-
-    # Same as events here
-
-    # Every value in the dict is an int value
-    # {HD : 20, HDDT : 60, ...}
-    #self.playrate    = {}
-
-    # Every value in the dict is an int value
-    # mods :  beatmaps id recommended in this mods
-    #  {HD : [123456, 456789, 132456, ...], HDDT : [...], ...}
-    #self.recommended = {}
+    username           = db.Column(db.String ())
+    country            = db.Column(db.String ())
+    last_patch_used    = db.Column(db.String ())
+    playrate           = db.Column(db.String ())
+    recommended        = db.Column(db.String ())
+    ranked_score       = db.Column(db.Float  ())
+    total_score        = db.Column(db.Float  ())
+    level              = db.Column(db.Float  ())
+    pp_raw             = db.Column(db.Float  ())
+    accuracy           = db.Column(db.Float  ())
+    pp_average         = db.Column(db.Float  ())
+    bpm_low            = db.Column(db.Float  ())
+    bpm_average        = db.Column(db.Float  ())
+    bpm_high           = db.Column(db.Float  ())
+    od_average         = db.Column(db.Float  ())
+    ar_average         = db.Column(db.Float  ())
+    cs_average         = db.Column(db.Float  ())
+    len_average        = db.Column(db.Float  ())
+    playstyle          = db.Column(db.Float  ())
+    last_update        = db.Column(db.Date   ())
